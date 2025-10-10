342bf536e32ea4fac44ee7a94940c65b7372d0cd
uri zafrir <urizaf@gmail.com>
2025-10-10 18:41:18 +0300

    a

--- hosts/home-lab/configuration.nix
+++ hosts/home-lab/configuration.nix
@@ -346,7 +346,7 @@ services.xserver.enable = true;
       X11Forwarding = true;
     };
   };
-  # services.tailscale.enable = true; # sudo tailscale up --auth-key=
+  services.tailscale.enable = true; # sudo tailscale up --auth-key=
   services.k3s = {
     enable = false;
     role = "server";
